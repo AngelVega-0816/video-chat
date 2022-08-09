72870b6030b9c1841146cbc4b4ef27021b836903
angelvega0816 <angelvegaxdpro@hotmail.com>
2022-08-09 00:16:28 -0300

    cors

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("http://localhost:3001");
+const socket = io(process.env.VERCEL_URL || "http://localhost:3001/");
 
 const ContextProvider = ({ children }) => {
 	const [stream, setStream] = useState(null);
