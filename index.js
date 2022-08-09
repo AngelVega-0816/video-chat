f7481a5781329bd2ccdb092246ec0cedbb09f2b3
angelvega0816 <angelvegaxdpro@hotmail.com>
2022-08-09 12:46:28 -0300

    finish

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://video-chat--webrtc.vercel.app/",
-    // origin: "*",
+    // origin: "https://video-chat--webrtc.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
@@ -14,7 +14,8 @@ const io = require("socket.io")(server, {
 app.use(cors())
 
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://video-chat--webrtc.vercel.app/");
+	// res.header("Access-Control-Allow-Origin", "https://video-chat--webrtc.vercel.app/");
+	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header(
 		"Access-Control-Allow-Headers",
