0834daaa8ec313170138c7e78c569c33eb04c923
angelvega0816 <angelvegaxdpro@hotmail.com>
2022-08-09 12:34:13 -0300

    dmeo

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001
 const io = require("socket.io")(server, {
   cors: {
-    // origin: "https://video-chat--webrtc.vercel.app/",
-    origin: "*",
-    // methods: ["GET", "POST"]
+    origin: "https://video-chat--webrtc.vercel.app/",
+    // origin: "*",
+    methods: ["GET", "POST"]
   }
 });
 
 app.use(cors())
 
-// app.use((req, res, next) => {
-// 	res.header("Access-Control-Allow-Origin", "https://video-chat--webrtc.vercel.app");
-// 	res.header("Access-Control-Allow-Credentials", "true");
-// 	res.header(
-// 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Accept"
-// 	);
-// 	res.header("Access-Control-Allow-Methods", "GET, POST");
-// 	next();
-// });
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "https://video-chat--webrtc.vercel.app");
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST");
+	next();
+});
 
 // app.get("/demo", (req, res) => {
 //   res.send("hoola")
@@ -45,4 +45,4 @@ io.on("connection", (socket) => {
 
 })
 
-server.listen(process.env.PORT, () => console.log("Server listening on port " + PORT))
+server.listen(PORT, () => console.log("Server listening on port " + PORT))
