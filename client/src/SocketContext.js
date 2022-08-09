ede2475499f5b2527bf4ec5eb41002ece26cdfba
angelvega0816 <angelvegaxdpro@hotmail.com>
2022-08-09 12:00:06 -0300

    cors

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io("https://webrtcvideo-chat.herokuapp.com/", {
+const socket = io("https://webrtcvideo-chat.herokuapp.com", {
   withCredentials: true,
 });
 
--- index.js
+++ index.js
@@ -5,25 +5,26 @@ const cors = require("cors");
 const PORT = process.env.PORT || 3001
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://video-chat--webrtc.vercel.app/",
+    // origin: "https://video-chat--webrtc.vercel.app/",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
 
 app.use(cors())
 
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://video-chat--webrtc.vercel.app");
-	res.header("Access-Control-Allow-Credentials", "true");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	res.header("Access-Control-Allow-Methods", "GET, POST");
-	next();
-});
+// app.use((req, res, next) => {
+// 	res.header("Access-Control-Allow-Origin", "https://video-chat--webrtc.vercel.app");
+// 	res.header("Access-Control-Allow-Credentials", "true");
+// 	res.header(
+// 		"Access-Control-Allow-Headers",
+// 		"Origin, X-Requested-With, Content-Type, Accept"
+// 	);
+// 	res.header("Access-Control-Allow-Methods", "GET, POST");
+// 	next();
+// });
 
-app.get("/", (req, res) => {
+app.get("/demo", (req, res) => {
   res.send("hi")
 })
 
